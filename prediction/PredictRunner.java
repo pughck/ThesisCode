785cb951199caa5ab8428b8e26a3862db6e5e057
Cort Pugh <pughck@rose-hulman.edu>
2016-03-17 10:54:24 -0400

    updated to write results for each type

--- prediction/PredictRunner.java
+++ prediction/PredictRunner.java
@@ -19,15 +19,39 @@ public class PredictRunner {
 					basePath + "of/" + args[i] + "/testing-" + totalMin + ".txt",
 					basePath + "of/results-" + totalMin + ".txt");
 			predict.predict();
+			predict = new Predict(basePath + "of/training-" + totalMin + "-basic.txt",
+					basePath + "of/" + args[i] + "/testing-" + totalMin + "-basic.txt",
+					basePath + "of/results-" + totalMin + "-basic.txt");
+			predict.predict();
+			predict = new Predict(basePath + "of/training-" + totalMin + "-related.txt",
+					basePath + "of/" + args[i] + "/testing-" + totalMin + "-related.txt",
+					basePath + "of/results-" + totalMin + "-related.txt");
+			predict.predict();
 
 			predict = new Predict(basePath + "stanford/training-" + totalMin + ".txt",
 					basePath + "stanford/" + args[i] + "/testing-" + totalMin + ".txt",
 					basePath + "stanford/results-" + totalMin + ".txt");
 			predict.predict();
+			predict = new Predict(basePath + "stanford/training-" + totalMin + "-basic.txt",
+					basePath + "stanford/" + args[i] + "/testing-" + totalMin + "-basic.txt",
+					basePath + "stanford/results-" + totalMin + "-basic.txt");
+			predict.predict();
+			predict = new Predict(basePath + "stanford/training-" + totalMin + "-related.txt",
+					basePath + "stanford/" + args[i] + "/testing-" + totalMin + "-related.txt",
+					basePath + "stanford/results-" + totalMin + "-related.txt");
+			predict.predict();
 
 			predict = new Predict(basePath + "open/training-" + totalMin + ".txt",
 					basePath + "open/" + args[i] + "/testing-" + totalMin + ".txt",
 					basePath + "open/results-" + totalMin + ".txt");
+			predict.predict();
+			predict = new Predict(basePath + "open/training-" + totalMin + "-basic.txt",
+					basePath + "open/" + args[i] + "/testing-" + totalMin + "-basic.txt",
+					basePath + "open/results-" + totalMin + "-basic.txt");
+			predict.predict();
+			predict = new Predict(basePath + "open/training-" + totalMin + "-related.txt",
+					basePath + "open/" + args[i] + "/testing-" + totalMin + "-related.txt",
+					basePath + "open/results-" + totalMin + "-related.txt");
 			predict.predict();
 		}
 	}
